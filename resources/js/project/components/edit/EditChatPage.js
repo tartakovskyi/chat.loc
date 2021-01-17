3bbdf6c65536e22aaf908697933afb2af07bb999
tartakovskyi <vvt2001@ukr.net>
2021-01-17 22:48:25 +0200

    participants

--- resources/js/project/components/edit/EditChatPage.js
+++ resources/js/project/components/edit/EditChatPage.js
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from 'react'
-// import { connect } from 'react-redux'
-// import { getChatInfoAction } from '../../store/actions/chatAction'
-//import PropTypes from 'prop-types'
-//import setFormObject from "../common/FormUtils"
+import React from 'react'
 import ChatForm from "./ChatForm"
+import ParticipantList from "./ParticipantList"
 
 
 const EditChatPage = ({match}) => {
@@ -14,8 +11,9 @@ const EditChatPage = ({match}) => {
 		<div className="container">
 			<div className="row justify-content-center">
 				<div className="col-md-9 col-lg-6 col-xl-5">
-					<h1 className="text-center">{id ? 'Edit Chat' : 'Add New Chat'}</h1>
+					<h1 className="text-center">{ id ? 'Edit Chat' : 'Add New Chat' }</h1>
 					<ChatForm id={id} />
+					{ id && <ParticipantList id={id} /> }
 				</div>
 			</div>
 		</div>
--- resources/js/project/components/edit/ParticipantList.js
+++ resources/js/project/components/edit/ParticipantList.js
@@ -1,28 +1,29 @@
 import React, { useState, useEffect } from 'react'
-// import { connect } from 'react-redux'
-// import { getChatInfoAction } from '../../store/actions/chatAction'
-//import PropTypes from 'prop-types'
-//import setFormObject from "../common/FormUtils"
-import ChatForm from "./ChatForm"
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import ParticipantList from "./ParticipantList"
 
 
-const ParticipantList = ({match}) => {
-
-	const id = match.params.id !== "undefined" ? match.params.id : null
+const ParticipantList = ({participants, id}) => {
 
 	return (
-		<div className="container">
-			<div className="row justify-content-center">
-				<div className="col-md-9 col-lg-6 col-xl-5">
-					<h1 className="text-center">{id ? 'Edit Chat' : 'Add New Chat'}</h1>
-					<ChatForm id={id} />
-					<h2>Add/Delete Participants</h2>
-					
-				</div>
-			</div>
+		<div className="participants">
+			<ul>
+				{ participants.map(participant => <Participant name={participant.name} key={participant.id} / > }	
+			</ul>
 		</div>
 	)
 }
 
 
-export default ParticipantList
+ParticipantList.propTypes = {
+	participants: PropTypes.array
+}
+
+const mapStateToProps = ({chat}) => {
+	return {
+		participants : chat.participants
+	}
+}
+
+export default connect(mapStateToProps)(ParticipantList)
